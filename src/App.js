47d23a43a9697a157a3dd5973395b99031aba00d
pogojam <rlb278@nau.edu>
2019-01-04 16:13:50 -0700

    removed unused dependencys

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import axio from 'axios'
 import Dropzone from 'react-dropzone'
 import XLSX from 'xlsx'
 import Slip from './Slip';
